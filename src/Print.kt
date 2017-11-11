7c706d9b76a72013f1bcf1db9ddae61af14dda82
RKarkala <rkarkala10@gmail.com>
2017-11-11 13:40:12 -0600

    Java and Kotlin Classes

---
+++ src/Print.kt
@@ -0,0 +1,8 @@
+class Print {
+    fun name(): String{
+        return "Hello World!!!"
+    }
+}
+fun main(args: Array<String>) {
+    println("Hello World")
+}
---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String args[]){
+        Print p = new Print();
+        String print = p.name();
+        System.out.println(print);
+    }
+}
