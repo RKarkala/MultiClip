dead23973bc19f3b18348f28dd9a13349865e285
RKarkala <rkarkala10@gmail.com>
2017-11-12 08:25:48 -0600

    Working version

--- src/GUI.java
+++ src/GUI.java
@@ -1,7 +1,9 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import kotlin.jvm.Throws;
+import sun.misc.BASE64Decoder;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
@@ -10,39 +12,46 @@ import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.util.List;
-import java.util.Observable;
 
 
 public class GUI  {
+    static boolean dontCreate = false;
     public static void main(String args[]){
 
         ListenerKt.listen();
+
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         Point location = MouseInfo.getPointerInfo().getLocation();
         JFrame frame = new JFrame("Your Copies");
+        frame.setSize((int)(screenSize.width*.4), (int)(screenSize.height*.7));
+        JPanel outer = new JPanel();
         JPanel panel = new JPanel(new BorderLayout());
+        panel.setPreferredSize(new Dimension((int)(screenSize.width*.2), (int)(screenSize.height*.5)));
         frame.isAlwaysOnTop();
         frame.setLocation(location.x, location.y - 50);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(panel);
+        frame.add(panel, BorderLayout.WEST);
         frame.setResizable(false);
-        frame.setSize((int)(screenSize.width*.2), (int)(screenSize.height*.4));
-        frame.setLocationRelativeTo(null);
+
         int lastLength = 0;
-        final List<Object> tempCopies = ListenerKt.getAllContents();
-        DefaultListModel<String> model = new DefaultListModel<>();
-        JList<String> listArea = new JList<>(model);
+        DefaultListModel<Object> model = new DefaultListModel<>();
+        JList<Object> listArea = new JList<>(model);
         listArea.setFont(new Font("Courier", Font.BOLD, screenSize.width / 100));
-        JScrollPane listScroller = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        listArea.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        listArea.setVisibleRowCount(0);
+        listArea.setCellRenderer(new DefaultListCellRenderer(){
+            public int getHorizontalAlignment() {
+                return CENTER;
+            }
+        });
+        JScrollPane listScroller = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        listArea.setLayoutOrientation(JList.VERTICAL);
         listScroller.setViewportView(listArea);
         panel.add(listScroller);
         frame.setVisible(true);
@@ -52,36 +61,59 @@ public class GUI  {
                 if (mouseEvent.getClickCount() == 2) {
                     int index = theList.locationToIndex(mouseEvent.getPoint());
                     if (index >= 0) {
-                        String p = theList.getModel().getElementAt(index).toString();
+                        String p = ListenerKt.getAllContents().get(index).toString();
+                        System.out.println(p);
+                            BASE64Decoder decoder = new BASE64Decoder();
+                            BufferedImage image = null;
+                            byte[]  imageByte;
+                            try{
+                                imageByte = decoder.decodeBuffer(p);
+                                ByteArrayInputStream bis = new ByteArrayInputStream(imageByte);
+                                image = ImageIO.read(bis);
+                                if(image==null){
+                                    throw new Exception("Problem");
+                                }
+                                TransferableImage i = new TransferableImage(image);
+                                ListenerKt.getClipboard().setContents(i, null);
+                            }catch (Exception e){
+                                System.out.println("In Exception");
+                                StringSelection s = new StringSelection(theList.getModel().getElementAt(index).toString());
+                                ListenerKt.getClipboard().setContents(s, null);
+                            }
 
-                        if(p.startsWith("BufferedImage")){
-                            BufferedImage image = (BufferedImage) theList.getModel().getElementAt(index);
-                            TransferableImage i = new TransferableImage(image);
-                            ListenerKt.getClipboard().setContents(i, null);
-                        }else{
-                            StringSelection s = new StringSelection(theList.getModel().getElementAt(index).toString());
-                            ListenerKt.getClipboard().setContents(s, null);
-                        }
 
                     }
                 }
             }
         };
-        listArea.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
-        listArea.setLayoutOrientation(JList.VERTICAL_WRAP);
-        listArea.setVisibleRowCount(-1);
         listArea.addMouseListener(mouseListener);
         while(true) {
 
             List<Object> allCopies = ListenerKt.getAllContents();
             try{
-                Thread.sleep(200);
+                Thread.sleep(250);
             }catch (Exception e){
 
             }
             if(allCopies.size()!=lastLength) {
                 lastLength = allCopies.size();
-                model.addElement(allCopies.get(allCopies.size()-1).toString());
+                String temp = allCopies.get(allCopies.size()-1).toString();
+                if(temp.matches("^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$")){
+                    BASE64Decoder decoder = new BASE64Decoder();
+                    BufferedImage image = null;
+                    byte[]  imageByte;
+                    try{
+                        imageByte = decoder.decodeBuffer(temp);
+                        ByteArrayInputStream bis = new ByteArrayInputStream(imageByte);
+                        image = ImageIO.read(bis);
+                        Image newImage = image.getScaledInstance((int)(screenSize.width*.15),(int)(screenSize.height*.15),Image.SCALE_REPLICATE);
+                        model.addElement(new ImageIcon(newImage));
+                    }catch (Exception e){
+
+                    }
+                }else {
+                    model.addElement(temp);
+                }
 
             }
         }
--- src/Listener.kt
+++ src/Listener.kt
@@ -1,3 +1,4 @@
+import GUI.dontCreate
 import java.awt.Image
 import java.awt.Toolkit
 import java.awt.datatransfer.*
@@ -8,6 +9,7 @@ import java.awt.datatransfer.DataFlavor
 import java.awt.datatransfer.UnsupportedFlavorException
 import java.awt.datatransfer.Transferable
 import java.io.ByteArrayOutputStream
+import java.nio.charset.Charset
 import javax.imageio.ImageIO
 
 val allContents: MutableList<Any> = mutableListOf()
@@ -21,42 +23,44 @@ fun listen():Unit{
     scheduler.schedule(object : TimerTask() {
         override fun run() {
             try {
-                val copiedString: Any = clipboard.getData(DataFlavor.stringFlavor)
+                var copiedString: Any = clipboard.getData(DataFlavor.stringFlavor)
+                val bytes = copiedString.toString().toByteArray()
+                copiedString = String(bytes, Charset.forName("UTF-8"))
                 if(allContents.size==0){
                     allContents.add(copiedString)
-                    //allContents.forEach { a -> print("$a, ") }
-                    //println()
+                    allContents.forEach { a -> print("$a, ") }
+                    println()
                 }else if(!allContents.contains(copiedString)){
                     allContents.add(copiedString)
-                   // allContents.forEach { a -> print("$a, ") }
-                   // println()
+                    allContents.forEach { a -> print("$a, ") }
+                    println()
                 }
             }catch(e : Exception){
                 try {
                     val image: BufferedImage = clipboard.getData(DataFlavor.imageFlavor) as BufferedImage
                     val baos : ByteArrayOutputStream = ByteArrayOutputStream()
-                    ImageIO.write(image, "jpg", baos)
+                    ImageIO.write(image, "png", baos)
                     val imageInByte = baos.toByteArray()
                     val encoded64 : String = Base64.getEncoder().encodeToString(imageInByte)
-                    if(!imageStrings.contains(encoded64)) {
-                        imageStrings.add(encoded64)
                         if (allContents.size == 0) {
                             allContents.add(encoded64)
-                            allContents.forEach { a -> print("$a, ") }
-                            println()
-                        } else if (!allContents.contains(image)) {
+                           // println(encoded64);
+                            // allContents.forEach { a -> print("$a, ") }
+                            // println()
+                        } else if (!allContents.contains(encoded64)) {
                             allContents.add(encoded64)
-                            allContents.forEach { a -> print("$a, ") }
-                            println()
+                            //println(encoded64);
+                            // allContents.forEach { a -> print("$a, ") }
+                            // println()
                         }
-                    }
+
                 }catch(f : Exception){
-                    f.printStackTrace()
+
                 }
             }
 
         }
-    }, 100, 100)
+    }, 500, 500)
 
 }
 
