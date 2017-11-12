ac903225fafdff7652a214956f47941613961ba6
RKarkala <rkarkala10@gmail.com>
2017-11-11 18:44:52 -0600

    Timer Version

--- src/Listener.kt
+++ src/Listener.kt
@@ -1,4 +1,4 @@
-import java.awt.*
+import java.awt.Toolkit
 import java.awt.datatransfer.*
 import java.util.*
 import java.util.TimerTask
@@ -6,23 +6,28 @@ import java.util.TimerTask
 
 
 fun main(args: Array<String>) {
-    val clips: MutableList<String> = mutableListOf()
-    val t = Timer()
-    t.schedule(object : TimerTask() {
+    var allContents: MutableList<String> = mutableListOf()
+
+    val scheduler : Timer = Timer()
+    scheduler.schedule(object : TimerTask() {
         override fun run() {
-            //println("Entered")
-            val userClipBoard = Toolkit.getDefaultToolkit().systemClipboard
-                val currentClip = userClipBoard.getData(DataFlavor.stringFlavor).toString()
-                if (clips.size == 0) {
-                    clips.add(currentClip)
-                } else {
-                    if (!clips[clips.size - 1].equals(currentClip)) {
-                        clips.add(currentClip)
-                    }
-                }
-            //clips.forEach { a -> print("$a,") }
+            val toolkit : Toolkit = Toolkit.getDefaultToolkit()
+            val clipboard : Clipboard = toolkit.systemClipboard
+            val copiedString : String = clipboard.getData(DataFlavor.stringFlavor).toString()
+            if(allContents.size==0){
+                allContents.add(copiedString);
+                allContents.forEach { a -> print("$a, ") }
+                println()
+            }else if(!allContents.contains(copiedString)){
+                allContents.add(copiedString);
+                allContents.forEach { a -> print("$a, ") }
+                println()
+            }
         }
-    }, 0, 1)
+    }, 100, 100)
+
 
 }
+
+
 
